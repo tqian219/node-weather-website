e11847fb62fd1671c5fef17770f439cd8c908081
Tom Qian <toqian@cisco.com>
2021-01-01 07:58:09 -0500

    Web-server added humdity datapoint

--- src/app.js
+++ src/app.js
@@ -54,7 +54,7 @@ app.get('/help', (req, res) => {
 app.get('/weather', (req, res) => {
     if (!req.query.address) {
         return res.send({
-            error: 'You must produce an address!'
+            error: 'You must provide an address!'
         })
     }
 
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longtitude, callback) => {
         } else if (body.error) {
             callback('Unable to find locaiton, Try another search.', undefined)
         } else {
-            callback (undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees.')
+            callback (undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees.' + 'The humidy is ' + body.current.humidity + '.')
         }
     })
 }
